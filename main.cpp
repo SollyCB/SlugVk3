4dbbfa544ee882230cbc864e8ebba0a94de7f74f
SollyCB <solomoncardenbrown@gmail.com>
2023-10-31 00:38:01 +0000

    update string function usage main.cpp

--- main.cpp
+++ main.cpp
@@ -33,8 +33,8 @@ int main() {
 
     // Load Shaders
     String basic_shader_files[] = {
-        get_string("shaders/basic.vert.spv"),
-        get_string("shaders/basic.frag.spv"),
+        cstr_to_string("shaders/basic.vert.spv"),
+        cstr_to_string("shaders/basic.frag.spv"),
     };
     gpu::Set_Allocate_Info set_allocate_info = insert_shader_set("basic", 2, basic_shader_files, &shader_map);
     gpu::Descriptor_Allocation basic_set_allocation = gpu::create_descriptor_sets(1, &set_allocate_info);
@@ -42,9 +42,9 @@ int main() {
     // Load Models
     gpu::model::Model_Allocators model_allocs = gpu::model::init_allocators();
 
-    String model_dir = get_string("models/cesium-man/");
+    String model_dir = cstr_to_string("models/cesium-man/");
 
-    String model_files[] = {get_string("CesiumMan.gltf")};
+    String model_files[] = {cstr_to_string("CesiumMan.gltf")};
 
     gpu::model::Static_Model model =
         load_static_model(&model_allocs, &model_files[0], &model_dir);
