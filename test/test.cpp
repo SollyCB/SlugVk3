dcdda362e0be848340dadf6712bdf8d66c89798d
SollyCB <solomoncardenbrown@gmail.com>
2023-12-13 20:10:21 +0000

    overhaul test system

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,9 @@
 #if TEST
 #include "test.hpp"
 
-static Test_Tracker s_Test_Tracker;
-Test_Tracker* get_test_tracker_instance() { return &s_Test_Tracker; }
+static Test_Suite s_Test_Suite;
+Test_Suite* get_test_suite_instance() { return &s_Test_Suite; }
+
 void load_tests() {
     std::cout << "Test Config:\n";
     if (TEST_LIST_BROKEN) {
@@ -17,122 +18,85 @@ void load_tests() {
     }
     std::cout << "\nBeginning Tests...\n";
 
-    Test_Tracker *test = get_test_tracker_instance();
-    init_dyn_array(&test->modules, 10);
+    Test_Suite *suite = get_test_suite_instance();
+    bool growable        = true;
+    bool temp            = false;
+
+    suite->tests         = new_array<Test>(128, growable, temp);
+    suite->modules       = new_array<Test_Module>(128, growable, temp);
+    suite->string_buffer = create_string_buffer(1024, temp, growable);
+
+    // Test_Tracker *test = get_test_tracker_instance();
+    // init_dyn_array(&test->modules, 10);
 }
 void end_tests() {
-    Test_Tracker *test = get_test_tracker_instance();
+    Test_Suite *test = get_test_suite_instance();
 
     bool all_pass  = true;
     bool no_skip   = true;
     bool no_broken = true;
-    for(int i = 0; i < test->modules.len; ++i) {
-        Test_Module mod = test->modules.data[i];
-        std::cout << string_buffer_to_cstr(&mod.info) << ": \n";
-        if (mod.failed_test_names.len  == 0 &&
-            mod.broken_test_names.len  == 0 &&
-            mod.skipped_test_names.len == 0 &&
-            mod.skipped_broken_test_names.len == 0)
-        {
-            std::cout << "All OK\n";
-            continue;
-        }
 
-        if (mod.failed_test_names.len) {
-            std::cout << RED << "FAILED " << NC << mod.failed_test_names.len << " test(s):\n";
-            for(int j = 0; j < mod.failed_test_names.len; ++j) {
-                std::cout << "    " << string_buffer_to_cstr(&mod.failed_test_names.data[j]) << '\n';
-            }
-            all_pass = false;
-        }
-
-        if (mod.broken_test_names.len) {
-            std::cout << "Contains "<< mod.broken_test_names.len << ' '<<  RED << "BROKEN " << NC " test(s)";
-            if (TEST_LIST_BROKEN) {
-                std::cout << ":\n";
-                for(int j = 0; j < mod.broken_test_names.len; ++j) {
-                    std::cout << "    " << string_buffer_to_cstr(&mod.broken_test_names.data[j]) << '\n';
-                }
-            } else { std::cout << '\n'; }
-
-            no_broken = false;
-        }
-        if (mod.skipped_test_names.len) {
-            std::cout << YELLOW << "Skipped " << NC << mod.skipped_test_names.len << " test(s)";
-            if (TEST_LIST_SKIPPED) {
-                std::cout << ":\n";
-                for(int j = 0; j < mod.skipped_test_names.len; ++j) {
-                    std::cout << "    " << string_buffer_to_cstr(&mod.skipped_test_names.data[j]) << '\n';
-                }
-            } else { std::cout << '\n'; }
-            no_skip = false;
-        }
-        if (mod.skipped_broken_test_names.len) {
-            std::cout << YELLOW << "Skipped " << NC << mod.skipped_broken_test_names.len << " BROKEN test(s)";
-            if (TEST_LIST_SKIPPED || TEST_LIST_BROKEN) {
-                std::cout << ":\n";
-                for(int j = 0; j < mod.skipped_broken_test_names.len; ++j) {
-                    std::cout << "    " << string_buffer_to_cstr(&mod.skipped_broken_test_names.data[j]) << '\n';
-                }
-            } else { std::cout << '\n'; }
+    for(u32 i = 0; i < test->modules.len; ++i) {
+        switch(test->modules.data[i].result) {
+        case TEST_RESULT_BROKEN:
+            println("%sMODULE BROKEN:%s", YELLOW, NC);
+            println("    %s", test->modules.data[i].info.str);
+            break;
+        case TEST_RESULT_SKIPPED:
+            println("%sMODULE SKIPPED:%s", YELLOW, NC);
+            println("    %s", test->modules.data[i].info.str);
+            break;
+        case TEST_RESULT_FAIL:
+            println("%sMODULE FAILED:%s", RED, NC);
+            println("    %s", test->modules.data[i].info.str);
+            break;
+        case TEST_RESULT_SUCCESS:
+            break;
         }
     }
 
-    // @Todo looping through everything again is lame but at least there is no branching
-    // but since all of these are c++ way heap allocated this doestn really matter lol
-    for(int i = 0; i < test->modules.len; ++i) {
-        Test_Module *mod = &test->modules.data[i];
-        for(int j = 0; j < mod->failed_test_names.len; ++j) {
-            kill_heap_string_buffer(&mod->failed_test_names.data[j]);
-        }
-        for(int j = 0; j < mod->broken_test_names.len; ++j) {
-            kill_heap_string_buffer(&mod->broken_test_names.data[j]);
-        }
-        for(int j = 0; j < mod->skipped_test_names.len; ++j) {
-            kill_heap_string_buffer(&mod->skipped_test_names.data[j]);
-        }
-        for(int j = 0; j < mod->skipped_broken_test_names.len; ++j) {
-            kill_heap_string_buffer(&mod->skipped_broken_test_names.data[j]);
-        }
-        kill_heap_string_buffer(&mod->info);
-        kill_dyn_array<Heap_String_Buffer>(&mod->failed_test_names);
-        kill_dyn_array<Heap_String_Buffer>(&mod->skipped_test_names);
-        kill_dyn_array<Heap_String_Buffer>(&mod->broken_test_names);
-        kill_dyn_array<Heap_String_Buffer>(&mod->skipped_broken_test_names);
-    }
-    kill_dyn_array<Test_Module>(&test->modules);
-
-    if (!no_skip)
+    if (test->skipped)
         std::cout << YELLOW << "TESTS WERE SKIPPED" << NC << '\n';
     else
         std::cout << GREEN << "NO TESTS SKIPPED" << NC << '\n';
 
-    if (!no_broken)
+    if (test->broken)
         std::cout << YELLOW << "TESTS ARE BROKEN" << NC << '\n';
     else
         std::cout << GREEN << "NO BROKEN TESTS" << NC << '\n';
 
-    if (!all_pass)
+    if (test->fail)
         std::cout << RED << "TESTS FAILED" << NC << '\n';
     else 
         std::cout << GREEN << "NO FAILURES" << NC << '\n';
+
+    destroy_string_buffer(&test->string_buffer);
+    free_array(&test->modules);
+    free_array(&test->tests);
+
+    reset_temp();
 }
 
 void begin_test_module(const char *name, Test_Module *mod, const char *function_name,
         const char *file_name, bool broken, bool skipped) 
 {
-    const char *info_strings[] = {
-        "[", function_name, ", ", file_name, "] ", name
-    };
+    *mod = {};
 
-    mod->info = build_heap_string_buffer(6, info_strings);
-    mod->skipped = skipped ? true : false;
-    mod->broken = broken ? true : false;
+    Test_Suite *suite = get_test_suite_instance();
+    mod->start = suite->tests.len;
 
-    init_dyn_array<Heap_String_Buffer>(&mod->failed_test_names,  10);
-    init_dyn_array<Heap_String_Buffer>(&mod->skipped_test_names, 10);
-    init_dyn_array<Heap_String_Buffer>(&mod->broken_test_names,  10);
-    init_dyn_array<Heap_String_Buffer>(&mod->skipped_broken_test_names, 10);
+    char info_buffer[256];
+    string_format(info_buffer, "[%s, %s] %s", function_name, file_name, name);
+    assert(strlen(info_buffer) < 255);
+
+    mod->info = string_buffer_get_string(&suite->string_buffer, info_buffer);
+    if (broken) {
+        mod->result = TEST_RESULT_BROKEN;
+    } else if (skipped) {
+        mod->result = TEST_RESULT_SKIPPED;
+    } else {
+        mod->result = TEST_RESULT_SUCCESS;
+    }
 }
-void end_test_module(Test_Module *mod) {}
+
 #endif // #if TEST
--- test/test.hpp
+++ test/test.hpp
@@ -11,225 +11,278 @@
 #include "../allocator.hpp"
 #include "../string.hpp"
 #include "../array.hpp"
+#include "../print.h"
 
 #include <iostream> // LAAAAAAAME!! @Todo refine this fucking test system lol...
 
+struct Test_Suite;
+Test_Suite* get_test_suite_instance();
+
 enum Test_Result {
-    FAIL    = 0,
-    PASS    = 0x01,
-    SKIPPED = 0x02,
-    BROKEN  = 0x04,
+    TEST_RESULT_SUCCESS = 0,
+    TEST_RESULT_FAIL = 1,
+    TEST_RESULT_SKIPPED = 2,
+    TEST_RESULT_BROKEN = 3,
 };
 
-// @Todo for now these are just heap string buffers to prevent code from interfering with tests
-// (like emptying the temp allocator at whatever stage). Need to give tests their own temp allocators
 struct Test_Module {
-    Dyn_Array<Heap_String_Buffer> failed_test_names;
-    Dyn_Array<Heap_String_Buffer> skipped_test_names;
-    Dyn_Array<Heap_String_Buffer> broken_test_names;
-    Dyn_Array<Heap_String_Buffer> skipped_broken_test_names;
-    Heap_String_Buffer info;
-    bool skipped;
-    bool broken;
+    String info;
+    Test_Result result;
+    u32 start;
+    u32 end;
 };
-struct Test_Tracker {
-    Dyn_Array<Test_Module> modules;
+
+struct Test {
+    String info;
+    Test_Result result;
+    u32 index;
 };
-Test_Tracker* get_test_tracker_instance();
+
+struct Test_Suite {
+    u32 fail;
+    u32 skipped;
+    u32 broken;
+    String_Buffer string_buffer;
+    Array<Test_Module> modules;
+    Array<Test> tests;
+};
+
 void load_tests();
 void end_tests();
 
-void begin_test_module(const char *name,
-        Test_Module *mod, const char *function_name,
-        const char *file_name, bool broken, bool skipped);
+void begin_test_module(const char *name, Test_Module *mod, const char *function_name, const char *file_name,
+                       bool broken, bool skipped);
 void end_test_module(Test_Module *mod);
 
-#define RED "\u001b[31;1m"
-#define GREEN "\u001b[32;1m"
+#define RED    "\u001b[31;1m"
+#define GREEN  "\u001b[32;1m"
 #define YELLOW "\u001b[33;1m"
-#define BLUE "\u001b[34;1m"
-#define NC "\u001b[0m"
+#define BLUE   "\u001b[34;1m"
+#define NC     "\u001b[0m"
 
 #if TEST_LIST_BROKEN
 #define TEST_MSG_BROKEN(name) \
-    std::cout << RED << "BROKEN TEST " << NC << "'" << name << "'\n";
+    println("%sBROKEN TEST '%s'", RED, NC, name)
 #else
 #define TEST_MSG_BROKEN(name)
 #endif
 
 #if TEST_LIST_SKIPPED
 #define TEST_MSG_SKIPPED(name) \
-    std::cout << YELLOW << "Skip Test " << NC << "'" << name << "'\n";
+    println("%sSkip Test%s '%s'")
 #define TEST_MSG_SKIP_BROKEN(name) \
-    std::cout << YELLOW << "Warning: Skipping Broken Test " << NC << "'" << name << "'\n";
+    println("%sWarning: Skipping Broken Test '%s'", YELLOW, NC, name)
 #else
 #define TEST_MSG_SKIPPED(name)
 #define TEST_MSG_SKIP_BROKEN(name)
 #endif
 
 #define SKIP_BROKEN_TEST_MSG(mod) \
-    std::cout << YELLOW << "Warning: Module Skips " << mod.skipped_broken_test_names.len <<  " Broken Tests...\n" << NC;
+    println("%sWarning: Module Skips %u Broken Tests...%s", YELLOW, mod.skipped_broken_test_names.len, NC)
 
+// @Todo Cannot print floats yet, and these may be floats...
 #define TEST_MSG_FAIL(name, arg1_name, arg2_name, arg1_val, arg2_val) \
     std::cout << RED << "FAILED TEST " << name << ": \n" << NC  \
-    << "    " << arg1_name << " = " << arg1_val << ", " << arg2_name << " = " << arg2_val << '\n';
+    << "    " << arg1_name << " = " << arg1_val << ", " << arg2_name << " = " << arg2_val << '\n'
+
+#define TEST_MSG_PASS println("%sOK%s", GREEN, NC)
 
 template<typename Arg1, typename Arg2>
 void test_eq(Test_Module *mod, const char *test_name, const char *function_name,
         const char *arg1_name, const char *arg2_name,
-        Arg1 arg1, Arg2 arg2, bool broken)
+        Arg1 arg1, Arg2 arg2, bool broken, int line_number, const char *file)
 {
+    Test_Suite *suite  = get_test_suite_instance();
+    Test_Result result;
+
+    char info[127];
+    string_format(info, "%s, %s(), line: %i", file, function_name, line_number);
+    String string = string_buffer_get_string(&suite->string_buffer, info);
+
     if (broken) {
-        TEST_MSG_BROKEN(test_name);
-        Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->broken_test_names);
-        *tmp = build_heap_string_buffer(1, &test_name);
-        return;
-    }
-
-    // @Todo more efficient to use copy heres rather than build
-    if (mod->skipped) {
-        if (broken) {
-            Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->skipped_broken_test_names);
-            *tmp = build_heap_string_buffer(1, &test_name); // a build (not a copy here - see above)
-        }
+        println("%s", string.str);
         TEST_MSG_SKIPPED(test_name);
-        Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->skipped_test_names);
-        *tmp = build_heap_string_buffer(1, &test_name);
-        return;
+        result      = TEST_RESULT_BROKEN;
+        mod->result = TEST_RESULT_BROKEN;
+        suite->broken++;
+    } else if (mod->result == TEST_RESULT_SKIPPED) {
+        println("%s", string.str);
+        TEST_MSG_BROKEN(test_name);
+        result      = TEST_RESULT_SKIPPED;
+        suite->skipped++;
+    } else if (arg1 == arg2) {
+        result = TEST_RESULT_SUCCESS;
+        //TEST_MSG_PASS;
+    } else {
+        println("%s", string.str);
+        result      = TEST_RESULT_FAIL;
+        mod->result = mod->result != TEST_RESULT_BROKEN ? TEST_RESULT_FAIL : TEST_RESULT_BROKEN;
+        TEST_MSG_FAIL(test_name, arg1_name, arg2_name, arg1, arg2);
+        suite->fail++;
     }
 
-    if (arg1 == arg2)
-        return;
+    if (broken) {result = TEST_RESULT_BROKEN;}
 
-    Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->failed_test_names);
-    *tmp = build_heap_string_buffer(1, &test_name);
-
-    TEST_MSG_FAIL(test_name, arg1_name, arg2_name, arg1, arg2);
+    if (result != TEST_RESULT_SUCCESS) {
+        Test test = {.info=string,.result=result,.index=suite->tests.len};
+        array_add(&suite->tests, &test);
+    }
 
     return;
 }
+
 template<typename Arg1, typename Arg2>
 void test_lt(Test_Module *mod, const char *test_name, const char *function_name,
         const char *arg1_name, const char *arg2_name,
-        Arg1 arg1, Arg2 arg2, bool broken)
-{
+        Arg1 arg1, Arg2 arg2, bool broken, int line_number, const char *file)
+{   
+    Test_Suite *suite  = get_test_suite_instance();
+    Test_Result result;
+
+    char info[127];
+    string_format(info, "%s, %s(), line: %i", file, function_name, line_number);
+    String string = string_buffer_get_string(&suite->string_buffer, info);
+
     if (broken) {
-        TEST_MSG_BROKEN(test_name);
-        Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->broken_test_names);
-        *tmp = build_heap_string_buffer(1, &test_name);
-        return;
-    }
-
-    // @Todo more efficient to use copy heres rather than build
-    if (mod->skipped) {
-        if (broken) {
-            Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->skipped_broken_test_names);
-            *tmp = build_heap_string_buffer(1, &test_name); // a build (not a copy here - see above)
-        }
+        println("%s", string.str);
         TEST_MSG_SKIPPED(test_name);
-        Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->skipped_test_names);
-        *tmp = build_heap_string_buffer(1, &test_name);
-        return;
+        result      = TEST_RESULT_BROKEN;
+        mod->result = TEST_RESULT_BROKEN;
+        suite->broken++;
+    } else if (mod->result == TEST_RESULT_SKIPPED) {
+        println("%s", string.str);
+        TEST_MSG_BROKEN(test_name);
+        result      = TEST_RESULT_SKIPPED;
+        suite->skipped++;
+    } else if (arg1 < arg2) {
+        result = TEST_RESULT_SUCCESS;
+        // TEST_MSG_PASS;
+    } else {
+        println("%s", string.str);
+        result      = TEST_RESULT_FAIL;
+        mod->result = mod->result != TEST_RESULT_BROKEN ? TEST_RESULT_FAIL : TEST_RESULT_BROKEN;
+        TEST_MSG_FAIL(test_name, arg1_name, arg2_name, arg1, arg2);
+        suite->fail++;
     }
 
-    if (arg1 < arg2)
-        return;
+    if (broken) {result = TEST_RESULT_BROKEN;}
 
-    Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->failed_test_names);
-    *tmp = build_heap_string_buffer(1, &test_name);
-
-    TEST_MSG_FAIL(test_name, arg1_name, arg2_name, arg1, arg2);
+    if (result != TEST_RESULT_SUCCESS) {
+        Test test = {.info=string,.result=result,.index=suite->tests.len};
+        array_add(&suite->tests, &test);
+    }
 
     return;
 }
+
 template<typename Arg1, typename Arg2>
 void test_floateq(Test_Module *mod, const char *test_name, const char *function_name,
         const char *arg1_name, const char *arg2_name,
-        Arg1 arg1, Arg2 arg2, bool broken)
-{
-    if (broken) {
-        TEST_MSG_BROKEN(test_name);
-        Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->broken_test_names);
-        *tmp = build_heap_string_buffer(1, &test_name);
-        return;
-    }
+        Arg1 arg1, Arg2 arg2, bool broken, int line_number, const char *file)
+{   
+    Test_Suite *suite  = get_test_suite_instance();
+    Test_Result result;
 
-    // @Todo more efficient to use copy heres rather than build
-    if (mod->skipped) {
-        if (broken) {
-            Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->skipped_broken_test_names);
-            *tmp = build_heap_string_buffer(1, &test_name); // a build (not a copy here - see above)
-        }
-        TEST_MSG_SKIPPED(test_name);
-        Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->skipped_test_names);
-        *tmp = build_heap_string_buffer(1, &test_name);
-        return;
-    }
+    char info[127];
+    string_format(info, "%s, %s(), line: %i", file, function_name, line_number);
+    String string = string_buffer_get_string(&suite->string_buffer, info);
 
     float f1 = arg1 - arg2;
     float f2 = arg2 - arg1;
     float inaccuracy = 0.000001;
-    if (f1 < inaccuracy && f2 < inaccuracy)
-        return;
 
-    Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->failed_test_names);
-    *tmp = build_heap_string_buffer(1, &test_name);
+    if (broken) {
+        println("%s", string.str);
+        TEST_MSG_SKIPPED(test_name);
+        result      = TEST_RESULT_BROKEN;
+        mod->result = TEST_RESULT_BROKEN;
+        suite->broken++;
+    } else if (mod->result == TEST_RESULT_SKIPPED) {
+        println("%s", string.str);
+        TEST_MSG_BROKEN(test_name);
+        result      = TEST_RESULT_SKIPPED;
+        suite->skipped++;
+    } else if (f1 < inaccuracy && f2 < inaccuracy) {
+        result = TEST_RESULT_SUCCESS;
+        // TEST_MSG_PASS;
+    } else {
+        println("%s", string.str);
+        result      = TEST_RESULT_FAIL;
+        mod->result = mod->result != TEST_RESULT_BROKEN ? TEST_RESULT_FAIL : TEST_RESULT_BROKEN;
+        TEST_MSG_FAIL(test_name, arg1_name, arg2_name, arg1, arg2);
+        suite->fail++;
+    }
 
-    TEST_MSG_FAIL(test_name, arg1_name, arg2_name, arg1, arg2);
+    if (broken) {result = TEST_RESULT_BROKEN;}
+
+    if (result != TEST_RESULT_SUCCESS) {
+        Test test = {.info=string,.result=result,.index=suite->tests.len};
+        array_add(&suite->tests, &test);
+    }
 
     return;
 }
+
 template<typename Arg1, typename Arg2>
 void test_streq(Test_Module *mod, const char *test_name, const char *function_name,
         const char *arg1_name, const char *arg2_name,
-        Arg1 arg1, Arg2 arg2, bool broken)
-{
+        Arg1 arg1, Arg2 arg2, bool broken, int line_number, const char *file)
+{   
+    Test_Suite *suite  = get_test_suite_instance();
+    Test_Result result;
+
+    char info[127];
+    string_format(info, "%s, %s(), line: %i", file, function_name, line_number);
+    String string = string_buffer_get_string(&suite->string_buffer, info);
+
     if (broken) {
-        TEST_MSG_BROKEN(test_name);
-        Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->broken_test_names);
-        *tmp = build_heap_string_buffer(1, &test_name);
-        return;
-    }
-
-    // @Todo more efficient to use copy heres rather than build
-    if (mod->skipped) {
-        if (broken) {
-            Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->skipped_broken_test_names);
-            *tmp = build_heap_string_buffer(1, &test_name); // a build (not a copy here - see above)
-        }
+        println("%s", string.str);
         TEST_MSG_SKIPPED(test_name);
-        Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->skipped_test_names);
-        *tmp = build_heap_string_buffer(1, &test_name);
-        return;
+        result      = TEST_RESULT_BROKEN;
+        mod->result = TEST_RESULT_BROKEN;
+        suite->broken++;
+    } else if (mod->result == TEST_RESULT_SKIPPED) {
+        println("%s", string.str);
+        TEST_MSG_BROKEN(test_name);
+        result      = TEST_RESULT_SKIPPED;
+        suite->skipped++;
+    } else if (strcmp(arg1, arg2) == 0) {
+        result = TEST_RESULT_SUCCESS;
+        // TEST_MSG_PASS;
+    } else {
+        println("%s", string.str);
+        result      = TEST_RESULT_FAIL;
+        mod->result = mod->result != TEST_RESULT_BROKEN ? TEST_RESULT_FAIL : TEST_RESULT_BROKEN;
+        TEST_MSG_FAIL(test_name, arg1_name, arg2_name, arg1, arg2);
+        suite->fail++;
     }
 
-    if (strcmp(arg1, arg2) == 0)
-        return;
+    if (broken) {result = TEST_RESULT_BROKEN;}
 
-    Heap_String_Buffer *tmp = append_to_dyn_array<Heap_String_Buffer>(&mod->failed_test_names);
-    *tmp = build_heap_string_buffer(1, &test_name);
-
-    TEST_MSG_FAIL(test_name, arg1_name, arg2_name, arg1, arg2);
+    if (result != TEST_RESULT_SUCCESS) {
+        Test test = {.info=string,.result=result,.index=suite->tests.len};
+        array_add(&suite->tests, &test);
+    }
 
     return;
 }
 
-#define BEGIN_TEST_MODULE(name, broken, skipped) \
-    Test_Module *test_module = append_to_dyn_array(&get_test_tracker_instance()->modules); \
-    begin_test_module(name, test_module, __FUNCTION__, __FILE__, broken, skipped);
+#define TEST_MODULES (&get_test_suite_instance()->modules)
+#define LAST_MODULE  (array_last(TEST_MODULES))
 
-// @Todo see if this needs to do anything... (currently does nothing)
-#define END_TEST_MODULE() \
-    end_test_module(test_module);
+#define BEGIN_TEST_MODULE(name, broken, skipped) \
+    begin_test_module(name, array_append(TEST_MODULES), __FUNCTION__, __FILE__, broken, skipped);
+
+#define END_TEST_MODULE() LAST_MODULE->end = (get_test_suite_instance()->tests.len - 1);
 
 #define TEST_EQ(test_name, arg1, arg2, broken) \
-    test_eq(test_module, test_name, __FUNCTION__, #arg1, #arg2, arg1, arg2, broken);
+    test_eq(LAST_MODULE, test_name, __FUNCTION__, #arg1, #arg2, arg1, arg2, broken, __LINE__, __FILE__);
 #define TEST_LT(test_name, arg1, arg2, broken) \
-    test_lt(test_module, test_name, __FUNCTION__, #arg1, #arg2, arg1, arg2, broken);
+    test_lt(LAST_MODULE, test_name, __FUNCTION__, #arg1, #arg2, arg1, arg2, broken, __LINE__, __FILE__);
 #define TEST_STREQ(test_name, arg1, arg2, broken) \
-    test_streq(test_module, test_name, __FUNCTION__, #arg1, #arg2, arg1, arg2, broken);
+    test_streq(LAST_MODULE, test_name, __FUNCTION__, #arg1, #arg2, arg1, arg2, broken, __LINE__, __FILE__);
 #define TEST_FEQ(test_name, arg1, arg2, broken) \
-    test_floateq(test_module, test_name, __FUNCTION__, #arg1, #arg2, arg1, arg2, broken);
+    test_floateq(LAST_MODULE, test_name, __FUNCTION__, #arg1, #arg2, arg1, arg2, broken, __LINE__, __FILE__);
 
 
 #endif // include guard
