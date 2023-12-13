80494717481bad8cd5585a670b11ce0208c34e8f
SollyCB <solomoncardenbrown@gmail.com>
2023-12-13 20:14:15 +0000

    update string buffer, remove old string code

--- string.cpp
+++ string.cpp
@@ -1,68 +1,80 @@
 #include "string.hpp"
 #include "allocator.hpp"
 
-Heap_String_Buffer build_heap_string_buffer(u32 cstr_count, const char **list_of_cstrs) {
+String_Buffer create_string_buffer(u32 size, bool temp, bool growable) {
+    String_Buffer string_buffer = {};
 
-    u32 total_len = 0;
-    u64 mark = get_mark_temp();
-    u32 *lens = (u32*)malloc_t(cstr_count * 4, 4);
+    string_buffer.flags |= growable ? STRING_BUFFER_GROWABLE_BIT : 0x0;
+    string_buffer.flags |= temp     ? STRING_BUFFER_TEMP_BIT     : 0x0;
 
-    for(int i = 0; i < cstr_count; ++i) {
-        lens[i] = 0;
-        for(int j = 0; list_of_cstrs[i][j] != '\0'; ++j) { 
-            total_len++;
-            lens[i]++;
+    string_buffer.cap = align(size, 16);
+
+    if (temp) {
+        string_buffer.buf = (char*)malloc_t(string_buffer.cap, 16);
+    } else {
+        string_buffer.buf = (char*)malloc_h(string_buffer.cap, 16);
+    }
+
+    return string_buffer;
+}
+
+void destroy_string_buffer(String_Buffer *buf) {
+    if ((buf->flags & STRING_BUFFER_TEMP_BIT) == 0)
+        free_h(buf->buf);
+    *buf = {};
+}
+
+static void string_buffer_do_resize(String_Buffer *string_buffer, u32 len);
+
+String string_buffer_get_string(String_Buffer *string_buffer, String *str) {
+    String string = {};
+    string.len = str->len;
+    string.str = (char*)(string_buffer->buf + string_buffer->len);
+
+    if (string_buffer->len + string.len + 1 >= string_buffer->cap)
+        string_buffer_do_resize(string_buffer, string.len + 1);
+
+    memcpy(string_buffer->buf + string_buffer->len, string.str, string.len); // copy null
+
+    string_buffer->len += string.len + 1;
+    string_buffer->buf[string_buffer->len - 1] = '\0';
+
+    return string;
+}
+
+String string_buffer_get_string(String_Buffer *string_buffer, const char *cstr) {
+    String string = {};
+    string.len = strlen(cstr);
+    string.str = (const char*)(string_buffer->buf + string_buffer->len);
+
+    if (string_buffer->len + string.len + 1 >= string_buffer->cap)
+        string_buffer_do_resize(string_buffer, string.len + 1);
+
+    memcpy(string_buffer->buf + string_buffer->len, cstr, string.len + 1); // copy null
+
+    string_buffer->len += string.len + 1;
+
+    return string;
+}
+
+static void string_buffer_do_resize(String_Buffer *string_buffer, u32 len) {
+    if (string_buffer->flags & STRING_BUFFER_GROWABLE_BIT) {
+
+        string_buffer->cap += len;
+        string_buffer->cap *= 2;
+        string_buffer->cap  = align(string_buffer->cap, 16);
+
+        if (string_buffer->flags & STRING_BUFFER_TEMP_BIT) {
+            char *old_data     = string_buffer->buf;
+            string_buffer->buf = (char*)malloc_t(string_buffer->cap, 16);
+
+            memcpy(string_buffer->buf, old_data, string_buffer->len - len);
+        } else {
+            string_buffer->buf = (char*)realloc_h(string_buffer->buf, string_buffer->cap);
         }
+    } else {
+        assert(false && "String Buffer Overflow");
+        *string_buffer = {}; // Crash
+        return;
     }
-
-    Heap_String_Buffer ret; 
-    init_heap_string_buffer(&ret, total_len);
-    for(int i = 0; i < cstr_count; ++i) {
-        copy_to_heap_string_buffer(&ret, (char*)list_of_cstrs[i], lens[i]);
-    }
-
-    reset_to_mark_temp(mark);
-    return ret;
-}
-
-Temp_String_Buffer build_temp_string_buffer(u32 cstr_count, const char **list_of_cstrs) {
-    u32 total_len = 0;
-    u64 mark = get_mark_temp();
-    u32 *lens = (u32*)malloc_t(cstr_count * 4, 4);
-
-    for(int i = 0; i < cstr_count; ++i) {
-        lens[i] = 0;
-        for(int j = 0; *(list_of_cstrs[i] + j) != '\0'; ++j) { 
-            total_len++;
-            lens[i]++;
-        }
-    }
-
-    Temp_String_Buffer ret; 
-    init_temp_string_buffer(&ret, total_len);
-    for(int i = 0; i < cstr_count; ++i) {
-        copy_to_temp_string_buffer(&ret, (char*)list_of_cstrs[i], lens[i]);
-    }
-
-    reset_to_mark_temp(mark);
-    return ret;
-}
-
-int init_heap_string_buffer(Heap_String_Buffer *string_buffer, u32 size) {
-    string_buffer->data = (char*)malloc_h(size + 1, 1);
-    string_buffer->len = 0;
-
-#if DEBUG
-    string_buffer->cap = size;
-#endif
-    return size + 1;
-}
-int init_temp_string_buffer(Temp_String_Buffer *string_buffer, u32 size) {
-    string_buffer->data = (char*)malloc_t(size + 1, 1);
-    string_buffer->len = 0;
-
-#if DEBUG
-    string_buffer->cap = size;
-#endif
-    return size + 1;
 }
--- string.hpp
+++ string.hpp
@@ -6,14 +6,11 @@
 #include "typedef.h"
 #include "assert.h"
 
-// @DebugVsRelease @CompileWarning Be careful using strings in systems where consistent size matters:
-// A string compiled in debug mode will be bigger than one compiled in release mode...
-// (Idk if this is a dumb optimisation and will just cause more problems than it is worth...)
-
 struct String {
     u32 len;
     const char *str;
 };
+
 inline static String cstr_to_string(const char *cstr) {
     String string;
     string.str = cstr;
@@ -21,107 +18,24 @@ inline static String cstr_to_string(const char *cstr) {
     return string;
 }
 
+enum String_Buffer_Flag_Bits {
+    STRING_BUFFER_GROWABLE_BIT = 0x01,
+    STRING_BUFFER_TEMP_BIT     = 0x02,
+};
+typedef u8 String_Buffer_Flags;
+
 struct String_Buffer {
     char *buf;
     u32 len;
     u32 cap;
-};
-inline static String_Buffer create_string_buffer(u32 size, bool temp = false) {
-    String_Buffer ret;
-    ret.len = 0;
-    size = align(size, 16); // Just in case of some simd
-    ret.cap = size;
-    if (temp)
-        ret.buf = (char*)malloc_t(size, 16);
-    else
-        ret.buf = (char*)malloc_h(size, 16); // static predict heap
-    return ret;
-}
-inline static void destroy_string_buffer(String_Buffer *buf) {
-    free_h(buf->buf);
-    *buf = {};
-}
-// make a temp string into one backed by a buffer
-inline static String string_buffer_get_string(String_Buffer *buf, String *str) {
-    String ret = {};
-    ret.len = str->len;
-    ret.str = (const char*)(buf->buf + buf->len);
-
-    buf->len += ret.len + 1; // +1 for null byte
-    assert(buf->len <= buf->cap && "String Buffer Overflow");
-    memcpy((char*)ret.str, str->str, ret.len); // copy null byte
-    char *tmp = (char*)ret.str;
-    tmp[ret.len] = '\0';
-
-    return ret;
-}
-inline static String string_buffer_get_string(String_Buffer *buf, const char *cstr) {
-    String ret = {};
-    ret.len = strlen(cstr);
-    ret.str = (const char*)(buf->buf + buf->len);
-
-    buf->len += ret.len + 1; // +1 for null byte
-    assert(buf->len <= buf->cap && "String Buffer Overflow");
-    memcpy((char*)ret.str, cstr, ret.len + 1); // copy null byte
-
-    return ret;
-}
-
-    /* Old Interface (still used by test.hpp) */
-struct Heap_String_Buffer {
-    u32 len;
-    char *data;
-#if DEBUG
-    u32 cap;
-#endif
+    String_Buffer_Flags flags;
 };
 
-struct Temp_String_Buffer {
-    u32 len;
-    char *data;
-#if DEBUG
-    u32 cap;
-#endif
-};
+// I am dumb for making the args order different to array... I will fix...
+String_Buffer create_string_buffer(u32 size, bool temp = false, bool growable = false);
+void          destroy_string_buffer(String_Buffer *buf);
 
-Heap_String_Buffer build_heap_string_buffer(u32 cstr_count, const char **list_of_cstrs);
-Temp_String_Buffer build_temp_string_buffer(u32 cstr_count, const char **list_of_cstrs);
-
-int init_heap_string_buffer(Heap_String_Buffer *string_buffer, u32 size);
-int init_temp_string_buffer(Temp_String_Buffer *string_buffer, u32 size);
-
-// Inlines
-inline void kill_heap_string_buffer(Heap_String_Buffer *string_buffer) {
-    free_h((void*)(string_buffer->data));
-}
-
-inline void copy_to_heap_string_buffer(Heap_String_Buffer *string_buffer, char *data,  u32 len) {
-#if DEBUG
-    assert(string_buffer->len + len <= string_buffer->cap && "String Buffer Overflow");
-#endif
-
-    memcpy(string_buffer->data + string_buffer->len, data, len);
-    string_buffer->len += len;
-
-}
-inline void copy_to_temp_string_buffer(Temp_String_Buffer *string_buffer, char *data,  u32 len) {
-#if DEBUG
-    assert(string_buffer->len + len <= string_buffer->cap && "String Buffer Overflow");
-#endif
-
-    memcpy(string_buffer->data + string_buffer->len, data, len);
-    string_buffer->len += len;
-}
-
-inline const char *string_buffer_to_cstr(Temp_String_Buffer *string_buffer) {
-    // Always safe
-    string_buffer->data[string_buffer->len] = '\0';
-    return string_buffer->data;
-}
-inline const char *string_buffer_to_cstr(Heap_String_Buffer *string_buffer) {
-    // Always safe
-    string_buffer->data[string_buffer->len] = '\0';
-    return string_buffer->data;
-}
+String string_buffer_get_string(String_Buffer *string_buffer, String *string);
+String string_buffer_get_string(String_Buffer *string_buffer, const char *cstr);
 
 #endif
